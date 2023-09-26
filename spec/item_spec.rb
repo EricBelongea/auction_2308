b337cc6c5e3a32e177ecaf3785bfb59b27a3afba
Eric Belongea <eric.belongea@gmail.com>
2023-09-26 09:32:17 -0600

    Item class bidding

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Item do
   before(:each) do
     @item1 = Item.new('Chalkware Piggy Bank')
     @item2 = Item.new('Bamboo Picture Frame')
+    @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
   end
 
   describe '#initialize' do
@@ -16,6 +18,27 @@ RSpec.describe Item do
     it 'has attributes' do
       expect(@item1.name).to eq("Chalkware Piggy Bank")
       expect(@item2.name).to eq('Bamboo Picture Frame')
+    end
+  end
+
+  describe '#adding bids' do
+    it 'creates variable' do
+      expect(@item1.bids).to eq({})
+    end
+
+    it 'add_bid' do
+      @item1.add_bid(@attendee1, 20)
+      expect(@item1.bids).to eq({@attendee1 => 20})
+      
+      @item1.add_bid(@attendee2, 22)
+      expect(@item1.bids).to eq({@attendee1 => 20, @attendee2 => 22})
+    end
+
+    it 'has a highest bid' do
+      @item1.add_bid(@attendee1, 20)
+      @item1.add_bid(@attendee2, 22)
+      
+      expect(@item1.current_high_bid).to eq(22)
     end
   end
 end
